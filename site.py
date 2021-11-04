1d30501fde369441a0420f72eeb68b1f7b0b85e4
jachos254 <jachowski.m@gmail.com>
2021-11-04 15:09:14 +0100

    comment

--- site.py
+++ site.py
@@ -1,13 +1,13 @@
-import flask
-import game
-
-app = flask.Flask(__name__)
-
-@app.route('/')
-
-def index():
-    return "haha, You can't use tkinter in web browser. YIKES"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# import flask
+# import game
+#
+# app = flask.Flask(__name__)
+#
+# @app.route('/')
+#
+# def index():
+#     return "haha, You can't use tkinter in web browser. YIKES"
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
